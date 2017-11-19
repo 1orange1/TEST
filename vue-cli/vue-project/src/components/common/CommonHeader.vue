5ea4e372ce33e9d56e349a2fb93757b0b27eb7a0
1orange1 <yangyang88827@163.com>
2017-11-19 14:25:38 +0800

    ..

---
+++ vue-cli/vue-project/src/components/common/CommonHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <button>首页 </button>
+    <h2>movie</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+//        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header{
+    height    : 1rem;
+    background: rgb(33, 150, 243);
+    display   : flex;
+    position  : fixed;
+    top       : 0;
+    width     : 100%;
+  }
+  .header button{
+    width : 1rem;
+    height: 1rem;
+    color : #fff;
+    float : left;
+  }
+  .header h2{
+    float      : left;
+    color      : #fff;
+    text-align : center;
+    flex       : 1;
+    text-indent: -1rem;
+  }
+
+</style>
---
+++ vue-cli/vue-project/src/components/movie/Movie.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='movie'>
+      <common-header></common-header>
+      <common-footer></common-footer>
+  </div>
+</template>
+<script>
+import CommonHeader from '../common/CommonHeader'
+import CommonFooter from '../common/CommonFooter'
+    export default {
+        data () {
+            return {
+            msg: 'Welcome to Your Vue.js App',
+            aa : "fafdsafds"
+            }
+        },
+        components:{
+            CommonHeader,
+            CommonFooter,
+//            MovieNav
+        }
+    }
+</script>
+<style>
+
+</style>
